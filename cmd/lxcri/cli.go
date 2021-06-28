f060843567a62b2855e6f5c9b3e2dade012781e9
Ruben Jenster <r.jenster@drachenfels.de>
2021-06-28 11:30:01 +0200

    Improve error reporting

--- cmd/lxcri/cli.go
+++ cmd/lxcri/cli.go
@@ -274,7 +274,11 @@ func main() {
 		clxc.Log.Error().Err(err).Dur("duration", cmdDuration).Msg("command failed")
 		clxc.Release()
 		// write diagnostics message to stderr for crio/kubelet
-		fmt.Fprintf(os.Stderr, "lxcri://%s %s\n", clxc.containerID, err)
+		if clxc.containerID == "" {
+			fmt.Fprintln(os.Stderr, err)
+		} else {
+			fmt.Fprintf(os.Stderr, "lxcri://%s %s\n", clxc.containerID, err)
+		}
 
 		// exit with exit status of executed command
 		var errExec execError
